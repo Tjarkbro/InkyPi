83b6be52f4454cef8c8ca531ebf6105c7ff25305
Tjark <tjark.thierfeldt@gmail.com>
2025-03-30 20:24:20 +0200

    Home assistant

--- src/plugins/homeassistant/homeassistant.py
+++ src/plugins/homeassistant/homeassistant.py
@@ -1,21 +1,26 @@
 from plugins.base_plugin.base_plugin import BasePlugin
 import requests
-from PIL import Image, ImageDraw, ImageFont
+import logging
+
+logger = logging.getLogger(__name__)
 
 class HomeAssistantPlugin(BasePlugin):
-    def generate_image(self, settings, device_config):
-        """ Erstellt ein Bild mit Daten von der Home Assistant API """
-        
-        ha_url = settings.get("ha_url", "http://192.168.178.122:8123")
+    def generate_settings_template(self):
+        template_params = super().generate_settings_template()
+        template_params['style_settings'] = True
+        return template_params
+    
+    def generate_image(self, settings, device_config):    
+        ha_url = settings.get("ha_url") # schon in HTML
         ha_token = settings.get("ha_token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2MmQzMmMwNTMzNmM0NTk2ODZiZmI3ZTlhYTg2MTY5ZiIsImlhdCI6MTc0MzM1Mzk2MiwiZXhwIjoyMDU4NzEzOTYyfQ.XqW6_a8fsSAhz7j77NClRY2V3Ofhffp4BhSDNVfpYPc")
         entities = settings.get("entities", {
-            "temp": "sensor.wohnzimmer_temperature",
-            "strom": "sensor.stromverbrauch",
+            "temp": "sensor.aqara_luftsensor_wohnzimmer_temperature",
+            "strom": "sensor.geschirrspuler_energy",
             "fenster": "binary_sensor.fenster_wohnzimmer"
         })
 
         headers = {"Authorization": f"Bearer {ha_token}", "Content-Type": "application/json"}
-        title = "Titel"
+        title = settings.get("title")
 
         # API-Anfragen
         temp = self.get_state(ha_url, headers, entities["temp"])
@@ -23,20 +28,25 @@ class HomeAssistantPlugin(BasePlugin):
         fenster = self.get_state(ha_url, headers, entities["fenster"])
 
         # Bild erstellen
+        dimensions = device_config.get_resolution()
+        if device_config.get_config("orientation") == "vertical":
+            dimensions = dimensions[::-1]
+
+        # Template-Parameter vorbereiten
         image_template_params = {
-            "title": title
+            "title": title,
+            "temperature": temp,
+            "power_usage": strom,
+            "window_status": "Offen" if fenster == "on" else "Geschlossen",
+            "plugin_settings": settings
         }
 
-        image = Image.new("RGB", (600, 448), (255, 255, 255))
-        draw = ImageDraw.Draw(image)
-        font = ImageFont.load_default()
-
-        # Daten auf das Bild zeichnen
-        draw.text((10, 10), f"🌡 Temperatur: {temp}°C", font=font, fill=0)
-        draw.text((10, 40), f"⚡ Stromverbrauch: {strom} kWh", font=font, fill=0)
-        draw.text((10, 70), f"🚪 Fenster: {'Offen' if fenster == 'on' else 'Geschlossen'}", font=font, fill=0)
+        image = self.render_image(dimensions, "homeassistant.html", "homeassistant.css", image_template_params)
 
         return image
+    
+  #  def parse_entities(self):
+
 
     def get_state(self, url, headers, entity_id):
         """ Ruft den aktuellen Zustand einer Entität ab """
