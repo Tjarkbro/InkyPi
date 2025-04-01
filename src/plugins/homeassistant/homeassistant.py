06b2d5403480bcfe691dfbc5eba3a2b32f844ec5
Tjark <tjark.thierfeldt@gmail.com>
2025-04-01 19:39:32 +0200

    Upgrade for setting entities by yourself

--- src/plugins/homeassistant/homeassistant.py
+++ src/plugins/homeassistant/homeassistant.py
@@ -1,10 +1,12 @@
 from plugins.base_plugin.base_plugin import BasePlugin
 import requests
+import pytz
 import logging
 from datetime import datetime
 
 logger = logging.getLogger(__name__)
 
+DEFAULT_TIMEZONE = "US/Eastern"
 UNITS = {
     "standard": {
         "temperature": "°C",
@@ -13,6 +15,7 @@ UNITS = {
         "energy": "kWh"
     }
 }
+# Idee: http://192.168.178.122:8123/api/config vewenden, um Config direkt daraus zu speisen
 
 class HomeAssistantPlugin(BasePlugin):
     def generate_settings_template(self):
@@ -25,16 +28,19 @@ class HomeAssistantPlugin(BasePlugin):
         ha_token = settings.get("ha_token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI2MmQzMmMwNTMzNmM0NTk2ODZiZmI3ZTlhYTg2MTY5ZiIsImlhdCI6MTc0MzM1Mzk2MiwiZXhwIjoyMDU4NzEzOTYyfQ.XqW6_a8fsSAhz7j77NClRY2V3Ofhffp4BhSDNVfpYPc")
 
         headers = {"Authorization": f"Bearer {ha_token}", "Content-Type": "application/json"}
-        title = settings.get("title")
-        units = settings.get('units')
+        title = settings.get("title", "Home Assistant")
+        units = settings.get('units', 'standard')
 
         numberRooms = int(settings.get('numberRooms', 1))  # Anzahl der Räume abrufen
         rooms_data = {}
-        for i in range(1, numberRooms + 1):  # Durch alle Räume iterieren
-            room_name = settings.get(f"room_name_{i}", f"Raum_{i}")  # Falls leer, Standardname
-            entity_temp = f"sensor.aqara_luftsensor_{room_name.lower()}_temperature"  # Sensor-Entity für Temperatur
-            entity_humidity = f"sensor.aqara_luftsensor_{room_name.lower()}_humidity"  # Sensor-Entity für Feuchtigkeit
-            entity_window = f"binary_sensor.aqara_fenstersensor_{room_name.lower()}_window_contact"  # Fensterstatus
+        for i in range(1, numberRooms + 1):
+            room_name = settings.get(f"room_name_{i}")
+            entity_temp = settings.get(f"sensor_temp_{i}")
+            entity_humidity = settings.get(f"sensor_humidity_{i}")
+            entity_window = settings.get(f"sensor_window_{i}")
+            # entity_temp = f"sensor.aqara_luftsensor_{room_name.lower()}_temperature"  # Sensor-Entity für Temperatur
+            # entity_humidity = f"sensor.aqara_luftsensor_{room_name.lower()}_humidity"  # Sensor-Entity für Feuchtigkeit
+            # entity_window = f"binary_sensor.aqara_fenstersensor_{room_name.lower()}_window_contact"  # Fensterstatus
 
             temp = self.get_state(ha_url, headers, entity_temp)
             humidity = self.get_state(ha_url, headers, entity_humidity)
@@ -54,7 +60,9 @@ class HomeAssistantPlugin(BasePlugin):
             dimensions = dimensions[::-1]
 
         # Aktuelles Datum und Uhrzeit abrufen
-        current_datetime = datetime.now()
+        timezone_name = device_config.get_config("timezone") or DEFAULT_TIMEZONE
+        tz = pytz.timezone(timezone_name)
+        current_datetime  = datetime.now(tz)
         formatted_date = current_datetime.strftime("%d.%m.%Y")  # Format: TT.MM.JJJJ
         formatted_time = current_datetime.strftime("%H:%M")  # Format: HH:MM
 
@@ -79,4 +87,5 @@ class HomeAssistantPlugin(BasePlugin):
             response.raise_for_status()
             return response.json().get("state", "N/A")
         except requests.RequestException as e:
+            logger.error(f"Fehler beim Abrufen des Zustands für {entity_id}: {e}")
             return f"Fehler: {e}"
